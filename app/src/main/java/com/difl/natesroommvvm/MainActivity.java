f3b876509c82c0ab83168ba8c6bb97e02d6fe4fa
ceatct <iamgrotesques@gmail.com>
2023-10-14 12:19:53 +0300

    123

--- app/src/main/java/com/difl/natesroommvvm/MainActivity.java
+++ app/src/main/java/com/difl/natesroommvvm/MainActivity.java
@@ -1,12 +1,9 @@
 package com.difl.natesroommvvm;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,30 +16,31 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottom = findViewById(R.id.bottom);
         bottom.setOnItemSelectedListener(navListener);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ListFragment()).commit();
+
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    @SuppressLint("NonConstantResourceId")
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
 
-            Fragment selectedFragment = null;
+        Fragment selectedFragment = null;
 
-            switch (item.getItemId()){
-                case R.id.main:
-                    selectedFragment = new ListFragment();
-                    break;
+        switch (item.getItemId()){
+            case R.id.main:
+                selectedFragment = new ListFragment();
+                break;
 
-                case R.id.add:
-                    selectedFragment = new InsertFragment();
-                    break;
-            }
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.container,
-                    selectedFragment).commit();
-
-            return true;
-
+            case R.id.add:
+                selectedFragment = new InsertFragment();
+                break;
         }
+
+        assert selectedFragment != null;
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                selectedFragment).commit();
+
+        return true;
+
     };
 
 }
